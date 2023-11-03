269711f646c7e7daebf5ce4563147b9dafac918e
jmlazaro <juan.lazaro2599@gmail.com>
2023-11-02 18:07:05 -0700

    resume v1.0

--- portfolio.py
+++ portfolio.py
@@ -1,24 +1,47 @@
 import streamlit as st
 import resume
-
+#import ldmx
 
 def main():
 
     st.set_page_config(
-        #layout='wide',
+        layout='wide',
         initial_sidebar_state='auto',
         menu_items=None
     )
 
-    st.title('Juan Manuel Lazaro Ruiz')
-    resume_tab, bird_class_tab = st.tabs([
-        'Resume :page_facing_up:',
-        'bird-class :penguin:'
-    ])
+    # layout TBD
+    left_col, main_col, right_col = st.columns([0.1, 100, 0.1])
 
-    with resume_tab:
-        resume.show_technical_skills()
-        resume.show_education()
+    with main_col:
+        st.title('Juan Manuel Lazaro Ruiz')
+        st.markdown(
+            ':e-mail: [juan.m.lazaro.ruiz@gmail.com](mailto:juan.m.lazaro.ruiz@gmail.com)'
+            ' | [![text](https://img.shields.io/badge/LinkedIn-0077B5?style=for-the-badge&logo=linkedin&logoColor=white)](https://www.linkedin.com/in/j-m-lazaro)'
+            ' | <a href="https://github.com/jmlazaro25"><img src="https://github.githubassets.com/assets/GitHub-Mark-ea2971cee799.png" alt="text" width="30rem"></a>',
+            unsafe_allow_html=True
+        )
+
+        resume_tab, ldmx_tab, bird_class_tab = st.tabs([
+            'Resume :page_facing_up:',
+            'LDMX :dark_sunglasses:',
+            'bird-class :penguin:'
+        ])
+
+        with resume_tab:
+            st.markdown(
+                '### 1. [Education](#education)\n'
+                '### 2. [Technical Skills](#technical-skills)\n'
+                '### 3. [Experience](#experience)\n'
+                '### 4. [Papers](#papers)\n'
+            )
+            resume.st_education()
+            resume.st_technical_skills()
+            resume.st_experience()
+            resume.st_papers()
+
+    #    with ldmx_tab:
+    #        ldmx.st_exec_summary()
 
 if __name__ == "__main__":
     main()
--- resume.py
+++ resume.py
@@ -1,41 +1,50 @@
 import streamlit as st
 
 
-def show_technical_skills():
+def st_left_right_align(left_text: str, right_text: str):
+    st.markdown(
+        '<div style="display: flex; justify-content: space-between;">'
+        f'<div style="font-size: 1.1em;"><b>{left_text}</b></div>'
+        f'<div style="font-size: 1.1em;"><b>{right_text}</b></div>'
+        '</div>',
+        unsafe_allow_html=True
+    )
+
+def st_technical_skills():
     st.header('Technical Skills')
     with st.expander('Show/Hide', expanded=True):
         st.subheader('Programming:')
         st.markdown("""
-        - __Python__
-            - __Numerical__: NumPy, SciPy, Statsmodels
-            - __Data__: Pandas, SQLAlchemy, requests, FastAPI
-            - __Machine Learning__: Scikit-Learn, PyTorch, TensorFlow, Hyperopt
-            - __Distributed computing__: PySpark
-            - __Visualizations__: Matplotlib, Seaborn, Jupyter, Streamliti
-        - __SQL__: PostgreSQL, MySQL, SQLite
-        - __Distributed Computing__: LSF, Slurm
-        - __Git/Github__
-        - __Linux__
-        - __Bash__
-        - __Docker__
-        - __Other__:
-            - Microsoft Excel, Microsoft PowerPoint
-        """)
+            - __Python__
+                - __Numerical__: NumPy, SciPy, Statsmodels
+                - __Data__: Pandas, SQLAlchemy, requests, FastAPI
+                - __Machine Learning__: Scikit-Learn, PyTorch, TensorFlow, Hyperopt
+                - __Distributed computing__: PySpark
+                - __Visualizations__: Matplotlib, Seaborn, Jupyter, Streamliti
+            - __SQL__: PostgreSQL, MySQL, SQLite
+            - __Distributed Computing__: LSF, Slurm
+            - __Git/Github__
+            - __Linux__
+            - __Bash__
+            - __Docker__
+            - __Other__:
+                - Microsoft Excel, Microsoft PowerPoint
+            """)
         st.subheader('Data Science and Machine Learning')
         st.markdown("""
-        - __Extract, transform, and load (ETL)__
-            - __Data Collection__
-            - __Data Wrangling__
-            - __Data Cleaning__
-        - __Data Analysis__
-        - __Modeling__
-            - Classification
-            - Regression
-            - Time Series
-            - Anomaly Detection
-        - __Data Visualization__
-        - __Model Deployment__
-        """)
+            - __Extract, transform, and load (ETL)__
+                - __Data Collection__
+                - __Data Wrangling__
+                - __Data Cleaning__
+            - __Data Analysis__
+            - __Modeling__
+                - Classification
+                - Regression
+                - Time Series
+                - Anomaly Detection
+            - __Data Visualization__
+            - __Model Deployment__
+            """)
         st.subheader('Industry Knowledge')
         st.markdown("""
             - __Research__
@@ -47,10 +56,77 @@ def show_technical_skills():
         """)
 
 
-def show_education():
+def st_education():
     st.header('Education')
     with st.expander('Show/Hide', expanded=True):
-        #st.subheader('Programming:')
-        st.latex(r"""\small
-        \text{The Data Incubator | Fellowship Program | Data Scientist Certification} \hfill \text{Sep 2023}
+        st_left_right_align(
+            'The Data Incubator'
+            ' | Fellowship Program'
+            ' | <a href="https://www.credly.com/badges/fb690af3-ce80-4a53-b6d6-7cb18e93fb8e/linked_in_profile" target="_blank">Data Scientist Certification</a>',
+            'Sep 2023'
+        )
+        st_left_right_align(
+            'DataCamp'
+            ' | <a href="https://www.datacamp.com/certificate/DS0023881595718" target="_blank">Data Science Professional Certification</a>'
+            ' | Valid through Mar 2025',
+            'Mar 2023'
+        )
+        st_left_right_align(
+            'Coursework toward Ph.D. in Physics | Massachusetts Institute of Technology (MIT)',
+            'Feb 2022 - Nov 2022'
+        )
+        st_left_right_align(
+            'B.S. in Physics | University of California, Santa Barbara (UCSB)',
+            'Sep 2021'
+        )
+        st.markdown("""
+            - NSF Graduate Research Fellowship Program Honorable Mention
+            - California Institute of Technology (Caltech) WAVE Fellowship
+            - UCSB McNair Fellowship
+            - UCSB Promise Scholar
+        """)
+
+def st_experience():
+    st.header('Experience')
+    with st.expander('Show/Hide', expanded=True):
+        st_left_right_align(
+            'MASSACHUSETTS INSTITUTE OF TECHNOLOGY',
+            '02/2022 – 11/2022'
+        )
+        st.markdown("""
+            ##### Graduate Fellow
+            - Studied magnetic field anomalies and their effects in new tokamak simulations to increase fusion efficiency and eventually establish commercial viability.
+            - Visualized and analyzed plasma to control instances of magnetic reconnection, optimizing tokamak’s performance.
+            - Tutored undergraduate individuals and groups in electromagnetism.
+        """)
+        st_left_right_align(
+            'UNIVERSITY OF CALIFORNIA, SANTA BARBARA',
+            '06/2019 – 09/2021'
+        )
+        st.markdown("""
+            ##### Undergraduate Researcher
+            - Contributed to rejection of all simulated background in Light Dark Matter eXperiment (LDMX) dark photon search by developing particle tracking algorithm and classification model (XGBoost) in electromagnetic calorimeter (ECal).
+            - Achieved 99%+ baseline efficiency for specific class of dark matter in LDMX by generating simulations of visible dark photons and creating new veto pipeline, confirming LDMX is sensitive to multiple classes of dark matter and therefore provides potential cost savings on future high-energy physics experiments.
+            - Updated up to 30 stakeholders in person and via Zoom on particle-tracking algorithm (weekly) and ML (every 2-3 weeks).
+        """)
+        st_left_right_align(
+            'CALIFORNIA INSTITUTE OF TECHNOLOGY',
+            '02/2020 – 08/2020'
+        )
+        st.markdown("""
+            ##### Undergraduate Research Fellow
+            - Spearheaded LDMX’s development of background rejection process for search of new class of dark matter (axion-like particles) by creating new XGBoost model utilizing more subdetectors than existing model for dark photon search.
+        """)
+
+def st_papers():
+    st.header('Papers')
+    with st.expander('Show/Hide', expanded=True):
+        st_left_right_align(
+            '<a href="https://link.springer.com/article/10.1007/JHEP04(2020)003" target="_blank">A high efficiency photon veto for the Light Dark Matter eXperiment</a>',
+            '04/2020'
+        )
+        st.markdown("""
+            Akesson, T., Blinov, N., __Lazaro, J.__, et al. (2020) | Journal of High Energy Physics, 3, 1-29
+            - Discusses particle tracking algorithm I helped develop which is the final step in rejecting all simulated background events.
+            - Includes visualizations I created.
         """)
